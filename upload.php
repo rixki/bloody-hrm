cf8146555392b0c9d349aa13adb984a15b2b1ecd
Muhammad Rizkiansyah <rixkiansyah@gmail.com>
2016-09-20 22:00:35 +0700

    Upload form

---
+++ upload.php
@@ -0,0 +1,10 @@
+<?php
+echo "this will be upload form for PDF files";
+
+echo "<form action='uploadfunc.php' method='post'>";
+
+echo "<input type='submit' value='Upload'>";
+
+echo "</form>"
+
+?>
